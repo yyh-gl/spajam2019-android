b5d95e03d657932cafa3cfd29eb2869b120ba10f
tshimakawa <tsukasa.soccer.0922@gmail.com>
2018-06-16 23:37:15 +0900

    crossFragmentにローディングダイアログを実装

--- spajam2019/app/src/main/java/com/isdl/spajam2019/Main/Fragment/Cross/CrossFragment.java
+++ spajam2019/app/src/main/java/com/isdl/spajam2019/Main/Fragment/Cross/CrossFragment.java
@@ -1,8 +1,10 @@
 package com.isdl.spajam2019.Main.Fragment.Cross;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -22,25 +24,7 @@ import java.util.List;
 
 import javax.inject.Inject;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link CrossFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link CrossFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class CrossFragment extends Fragment implements CrossContract.View {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
 
     @Inject
     CrossPresenter crossPresenter;
@@ -48,6 +32,8 @@ public class CrossFragment extends Fragment implements CrossContract.View {
     RecyclerView rv;
     CrossAdapter adapter;
     Context context;
+    private final Handler handler = new Handler();
+    private ProgressDialog progressDialog;
 
     private List<Music> musicList;
 
@@ -55,15 +41,6 @@ public class CrossFragment extends Fragment implements CrossContract.View {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CrossFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static CrossFragment newInstance() {
         return new CrossFragment();
     }
@@ -71,10 +48,6 @@ public class CrossFragment extends Fragment implements CrossContract.View {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
         DaggerActivityComponent.builder()
                 .appComponent(((Spajam2019Application) getActivity().getApplicationContext())
                         .getAppComponent())
@@ -82,13 +55,16 @@ public class CrossFragment extends Fragment implements CrossContract.View {
                 .build()
                 .inject(this);
 
+        progressDialog = new ProgressDialog(getContext());
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+        showLoadingDialog("","通信中");
+
         View root = inflater.inflate(R.layout.fragment_cross, container, false);
         context = root.getContext();
 
@@ -97,49 +73,21 @@ public class CrossFragment extends Fragment implements CrossContract.View {
         crossPresenter.getPossessedCrossMusic(2);
 
         return root;
-
-
     }
 
     @Override
     public void setAdapter(List<Music> possessedMusics) {
         adapter = new CrossAdapter(possessedMusics);
-
         LinearLayoutManager llm = new LinearLayoutManager(context);
-
         rv.setHasFixedSize(true);
-
         rv.setLayoutManager(llm);
-
         rv.setAdapter(adapter);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        dismissLoadingDialog();
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-    }
-
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 
     public List<User> createDataset() {
@@ -151,5 +99,19 @@ public class CrossFragment extends Fragment implements CrossContract.View {
             dataset.add(user);
         }
         return dataset;
+    }
+
+    public void showLoadingDialog(final String title, final String message) {
+        handler.post(() -> {
+            progressDialog.setTitle(title);
+            progressDialog.setMessage(message);
+            progressDialog.setCancelable(false);
+            progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            progressDialog.show();
+        });
+    }
+
+    public void dismissLoadingDialog(){
+        progressDialog.dismiss();
     }
 }
