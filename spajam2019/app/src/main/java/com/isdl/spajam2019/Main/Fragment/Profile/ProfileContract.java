cb06a0793fa2c4a84a29df48af7409cf4b1201ad
ytakaya <ytakaya@mikilab.doshisha.ac.jp>
2018-06-17 02:38:40 +0900

    user apiとの疎通確認

--- spajam2019/app/src/main/java/com/isdl/spajam2019/Main/Fragment/Profile/ProfileContract.java
+++ spajam2019/app/src/main/java/com/isdl/spajam2019/Main/Fragment/Profile/ProfileContract.java
@@ -2,10 +2,12 @@ package com.isdl.spajam2019.Main.Fragment.Profile;
 
 public interface ProfileContract {
     interface View {
+        void setUserInfo();
 
     }
 
     interface Presenter {
+        void getUserInfo(int userId);
 
     }
 }
